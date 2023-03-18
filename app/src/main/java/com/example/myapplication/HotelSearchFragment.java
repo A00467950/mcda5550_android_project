78b092030e5005a38cdb19db9f3736095d2314b3
Samuel Thiankee <thiankee.samuel@gmail.com>
2023-03-18 07:32:22 -0300

    Added hotel search layout and fragment.

---
+++ app/src/main/java/com/example/myapplication/HotelSearchFragment.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+
+public class HotelSearchFragment extends Fragment {
+    ConstraintLayout mainLayout;
+    View view;
+    TextView titleTextView;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.hotel_search_layout, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mainLayout = view.findViewById(R.id.main_layout);
+
+        titleTextView = view.findViewById(R.id.title_text_view);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Begin the transaction
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        // Replace the contents of the container with the new fragment
+        fragmentTransaction.replace(R.id.frame_layout, new HotelSearchFragment());
+
+        // Complete the changes added above
+        fragmentTransaction.commit();
     }
 }
